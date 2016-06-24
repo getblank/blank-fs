d37dd27ca481c1ca28e591b335cae8725aeadef2
ivahaev <ivahaev@gmail.com>
2016-06-24 11:01:13 +0500

    version support and debug mode

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
@@ -9,15 +10,28 @@ import (
 	"github.com/getblank/blank-fs/intranet"
 )
 
+var (
+	buildTime string
+	gitHash   string
+	version   = "0.0.5"
+)
+
 func main() {
-	log.SetLevel(log.DebugLevel)
+	if os.Getenv("BLANK_DEBUG") != "" {
+		log.SetLevel(log.DebugLevel)
+	}
 	var srAddress *string
 	var port *string
+	var verFlag *bool
 	rootCmd := &cobra.Command{
 		Use:   "blank-fs",
 		Short: "File storage microservice for Blank platform",
 		Long:  "The next generation of web applications. This is the file storage subsystem.",
 		Run: func(cmd *cobra.Command, args []string) {
+			if *verFlag {
+				printVersion()
+				return
+			}
 			log.Info("blank-fs started")
 			intranet.Init(*srAddress, *port)
 		},
@@ -25,9 +39,16 @@ func main() {
 
 	srAddress = rootCmd.PersistentFlags().StringP("service-registry", "s", "ws://localhost:1234", "Service registry uri")
 	port = rootCmd.PersistentFlags().StringP("port", "p", "8082", "TCP port to listen")
+	verFlag = rootCmd.PersistentFlags().BoolP("version", "v", false, "Prints version and exit")
 
 	if err := rootCmd.Execute(); err != nil {
 		println(err.Error())
 		os.Exit(-1)
 	}
+}
+
+func printVersion() {
+	fmt.Printf("Build time:  		%s\n", buildTime)
+	fmt.Printf("Commit hash: 		%s\n", gitHash)
+	fmt.Printf("Version:     		%s\n", version)
 }
