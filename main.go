8ddb85b86a3b3300d625263edad8f162d849dcd6
Evgeniy Ivakha <ivahaev@gmail.com>
2017-05-10 22:57:54 +0500

    cobra removed

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/spf13/cobra"
 	"gopkg.in/gemnasium/logrus-graylog-hook.v2"
+
+	"flag"
 
 	"github.com/getblank/blank-fs/intranet"
 )
@@ -36,31 +37,18 @@ func main() {
 		log.AddHook(hook)
 	}
 
-	var srAddress *string
-	var port *string
-	var verFlag *bool
-	rootCmd := &cobra.Command{
-		Use:   "blank-fs",
-		Short: "File storage microservice for Blank platform",
-		Long:  "The next generation of web applications. This is the file storage subsystem.",
-		Run: func(cmd *cobra.Command, args []string) {
-			if *verFlag {
-				printVersion()
-				return
-			}
-			log.Info("blank-fs started")
-			intranet.Init(*srAddress, *port)
-		},
+	srAddress := flag.String("s", "ws://localhost:1234", "Service registry uri")
+	port := flag.String("p", "8082", "TCP port to listen")
+	verFlag := flag.Bool("v", false, "Prints version and exit")
+	flag.Parse()
+
+	if *verFlag {
+		printVersion()
+		return
 	}
 
-	srAddress = rootCmd.PersistentFlags().StringP("service-registry", "s", "ws://localhost:1234", "Service registry uri")
-	port = rootCmd.PersistentFlags().StringP("port", "p", "8082", "TCP port to listen")
-	verFlag = rootCmd.PersistentFlags().BoolP("version", "v", false, "Prints version and exit")
-
-	if err := rootCmd.Execute(); err != nil {
-		println(err.Error())
-		os.Exit(-1)
-	}
+	log.Info("blank-fs started")
+	intranet.Init(*srAddress, *port)
 }
 
 func printVersion() {
