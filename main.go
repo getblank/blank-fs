9eb27c16250f8e266de92d4c8806e963f2f26c51
Evgeniy Ivakha <ivahaev@gmail.com>
2018-12-26 16:49:28 +0500

    dependencies updated gralog support removed

--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	log "github.com/Sirupsen/logrus"
-	"gopkg.in/gemnasium/logrus-graylog-hook.v2"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/getblank/blank-fs/intranet"
 )
@@ -22,19 +21,6 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 	log.SetFormatter(&log.JSONFormatter{})
-	if os.Getenv("GRAYLOG2_HOST") != "" {
-		host := os.Getenv("GRAYLOG2_HOST")
-		port := os.Getenv("GRAYLOG2_PORT")
-		if port == "" {
-			port = "12201"
-		}
-		source := os.Getenv("GRAYLOG2_SOURCE")
-		if source == "" {
-			source = "blank-fs"
-		}
-		hook := graylog.NewGraylogHook(host+":"+port, map[string]interface{}{"source-app": source})
-		log.AddHook(hook)
-	}
 
 	srAddress := flag.String("s", "ws://localhost:1234", "Service registry uri")
 	port := flag.String("p", "8082", "TCP port to listen")
